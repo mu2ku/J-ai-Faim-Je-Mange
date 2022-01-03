3cecec4f69d21e3662a205f3e1eba4ade5047187
mu2ku <gmutuku@gmail.com>
2022-01-03 10:51:09 -0500

    commit create for new recipes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+  strMeal = models.CharField(max_length=255)
+  strCategory = models.CharField(max_length=255)
+  strArea= models.CharField(max_length=255)
+  strInstructions = models.TextField()
+  strThumb = models.CharField(max_length=255, default="https://picsum.photos/200/300.jpg")
+  
+  def __str__(self):
+    return self.strMeal
+  
+  
+  
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Recipe
 import requests
 
 def home(request):
@@ -7,190 +8,19 @@ def home(request):
 def recipes_index(request):
   response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=a')
   recipes = response.json()
+  dbrecipes = Recipe.objects.all()
   return render(request, 'recipes/index.html', {
       'recipes': recipes['meals'],
-  })
-  
-def recipes_index_a(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=a')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
+      'dbrecipes': dbrecipes,
   })
 
-def recipes_index_b(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=b')
+def recipes_index_letter(request, letter):
+  response = requests.get(f'http://www.themealdb.com/api/json/v1/1/search.php?f={letter}')
   recipes = response.json()
+  dbrecipes = Recipe.objects.filter(strMeal__istartswith=letter)
   return render(request, 'recipes/index.html', {
       'recipes': recipes['meals'],
-  })
-  
-def recipes_index_c(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=c')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-
-def recipes_index_d(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=d')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_e(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=e')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_f(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=f')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_g(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=g')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_h(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=h')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_i(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=i')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_j(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=j')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_k(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=k')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_l(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=l')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_m(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=m')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_n(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=n')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_o(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=o')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_p(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=p')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_q(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=q')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_r(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=r')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_s(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=s')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_t(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=t')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_u(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=u')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_v(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=v')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_w(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=w')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_x(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=x')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_y(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=y')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-  })
-  
-def recipes_index_z(request):
-  response = requests.get('http://www.themealdb.com/api/json/v1/1/search.php?f=z')
-  recipes = response.json()
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
+      'dbrecipes': dbrecipes,
   })
   
 def recipes_detail(request, recipe_name):
