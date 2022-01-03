64d58395c3acf7207e512ff6cd09976c7d66ad49
mu2ku <gmutuku@gmail.com>
2022-01-03 00:48:47 -0500

    commit initial templates, urls, and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def recipes(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'recipes/recipes_index.html')
