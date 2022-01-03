425a1fc9e784af519b1a203001e92e3e4a1d78a8
mu2ku <gmutuku@gmail.com>
2022-01-03 13:51:41 -0500

    commit functionality to add recipes

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('recipes/', views.recipes_index, name='recipes_index'),
   path('recipes/<letter>', views.recipes_index_letter, name='recipes_index_letter'),
-  path('recipes/<recipe_name>/', views.recipes_detail,name='recipes_details'),
+  path('recipes/details/<recipe_name>/', views.recipes_detail,name='recipes_details'),
+  path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Recipe
 import requests
 
@@ -15,17 +16,36 @@ def recipes_index(request):
   })
 
 def recipes_index_letter(request, letter):
-  response = requests.get(f'http://www.themealdb.com/api/json/v1/1/search.php?f={letter}')
-  recipes = response.json()
-  dbrecipes = Recipe.objects.filter(strMeal__istartswith=letter)
-  return render(request, 'recipes/index.html', {
-      'recipes': recipes['meals'],
-      'dbrecipes': dbrecipes,
-  })
+  try:
+    response = requests.get(f'http://www.themealdb.com/api/json/v1/1/search.php?f={letter}')
+    recipes = response.json()
+    dbrecipes = Recipe.objects.filter(strMeal__istartswith=letter)
+    return render(request, 'recipes/index.html', {
+        'recipes': recipes['meals'],
+        'dbrecipes': dbrecipes,
+    })
+  except:
+    pass
   
 def recipes_detail(request, recipe_name):
-  response = requests.get(f"http://www.themealdb.com/api/json/v1/1/search.php?s={recipe_name}")
-  recipe = response.json()
-  return render(request, 'recipes/detail.html', {
-      'recipe': recipe['meals'][0],
-  })
+  try:
+    response = requests.get(f"http://www.themealdb.com/api/json/v1/1/search.php?s={recipe_name}")
+    
+    if(response is None):
+      dbrecipe = Recipe.objects.filter(strMeal=recipe_name)
+      return render(request, 'recipes/detail.html', {
+          'dbrecipe': dbrecipe,
+    })
+    else:
+      recipe = response.json()
+      return render(request, 'recipes/detail.html', {
+        'recipe': recipe['meals'][0],
+    })
+      
+  except Exception as e:
+    print(e)
+    
+class RecipeCreate(CreateView):
+  model = Recipe
+  fields = '__all__'
+  success_url = '/recipes/'
